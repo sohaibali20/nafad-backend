1b3b6d99f96514cf27f3f59cce7d1c4051a71609
Muhammad Ismaeel Zeb <109311287+ismaeel06@users.noreply.github.com>
2024-08-09 15:39:32 +0500

    Added attendance,dailysales,staff and ticket models

---
+++ src/models/attendance.model.js
@@ -0,0 +1,24 @@
+import mongoose,{Schema} from 'mongoose';
+
+const attendanceSchema = new Schema(
+{
+  buyer: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  event: {
+    type: Schema.Types.ObjectId,
+    ref: 'Event',
+    required: true,
+  },
+  attendancestatus: {
+    type: String,
+    enum: ['present', 'absent'], // Attendance status
+    default: 'absent',
+  } 
+},
+{ timestamps: true }
+);
+
+const Attendance = mongoose.model('Attendance', attendanceSchema);
