18cba651b35e0de9ddea1b9cdebd950fd6e28ced
sohaibali20 <sohaibalix@gmail.com>
2024-08-13 12:50:33 +0500

    review

---
+++ src/models/reviews.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+
+const reviewSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    Avatar: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      index: true,
+      unique: true,
+    },
+    events: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Event",
+      },
+    ],
+    stars: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Review = mongoose.model("review", reviewSchema);
+
+export default Review;
